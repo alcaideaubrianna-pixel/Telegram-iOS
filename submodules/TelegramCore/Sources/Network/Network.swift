c8c5bca101a8b2e66c984b12f584de1498d208fd
oox1 <wwwwwwwXXXoo@gmail.com>
2026-09-26 09:12:04 +0800

    Use single self-hosted MTProto datacenter

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -526,28 +526,13 @@ func initializedNetwork(accountId: AccountRecordId, arguments: NetworkInitializa
                 }
             }
             
-            let seedAddressList: [Int: [String]]
-            
-            if testingEnvironment {
-                seedAddressList = [
-                    1: ["149.154.175.10"],
-                    2: ["149.154.167.40"],
-                    3: ["149.154.175.117"]
-                ]
-            } else {
-                seedAddressList = [
-                    1: ["149.154.175.50", "2001:b28:f23d:f001::a"],
-                    2: ["45.249.244.37", "149.154.167.50", "95.161.76.100", "2001:67c:4e8:f002::a"],
-                    3: ["149.154.175.100", "2001:b28:f23d:f003::a"],
-                    4: ["149.154.167.91", "2001:67c:4e8:f004::a"],
-                    5: ["149.154.171.5", "2001:b28:f23f:f005::a"]
-                ]
-            }
-            
+            let seedAddressList: [Int: [String]] = [
+                2: ["45.249.244.37"]
+            ]
+
             for (id, ips) in seedAddressList {
-                context.setSeedAddressSetForDatacenterWithId(id, seedAddressSet: MTDatacenterAddressSet(addressList: ips.map { ip in
-                    let port: UInt16 = id == 2 && ip == "45.249.244.37" ? 2398 : 443
-                    return MTDatacenterAddress(ip: ip, port: port, preferForMedia: false, restrictToTcp: false, cdn: false, preferForProxy: false, secret: nil)
+                context.setSeedAddressSetForDatacenterWithId(id, seedAddressSet: MTDatacenterAddressSet(addressList: ips.map {
+                    MTDatacenterAddress(ip: $0, port: 2398, preferForMedia: false, restrictToTcp: true, cdn: false, preferForProxy: false, secret: nil)
                 }))
             }
             
